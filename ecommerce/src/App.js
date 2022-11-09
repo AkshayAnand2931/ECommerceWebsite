16b406754395b524e733dc847b1b7378b6407bac
chiman-09 <achintyakrishna2021@gmail.com>
2022-11-09 22:17:17 +0530

    Navbar and Routes

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route,Routes} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Men from './Components/Men';
+import Women from './Components/Women';
+import Shoes from './Components/Shoes';
+import Profile from './Components/Profile';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/men' element={<Men/>}></Route>
+          <Route path='/women' element={<Women/>}></Route>
+          <Route path='/shoes' element={<Shoes/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
