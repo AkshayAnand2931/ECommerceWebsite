c4f298662a39acbfaec5069eec60740ac17eca00
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2022-11-27 12:28:55 +0530

    local storage

--- ecommerce/src/Components/Bag.js
+++ ecommerce/src/Components/Bag.js
@@ -4,16 +4,22 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useNavigate } from "react-router";
 import ListGroup from "react-bootstrap/ListGroup";
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import DeleteIcon from '@mui/icons-material/Delete';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 
 function Bag() {
-  const { state, ctxDispatch } = useContext(Store);
+  const { state, dispatch:ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
   const navigate = useNavigate();
+  
+  const removeItemHandler = (items) =>
+  {
+    ctxDispatch({type:'CART_REMOVE_ITEM',payload:items})
+  }
 
   return (
     <div>
@@ -21,13 +27,15 @@ function Bag() {
       <Row>
         <Col md={8}>
           {cartItems.length === 0 ? (
-            <h1>
-              No Items in Cart.<a onClick={()=>navigate('/')}>Go Shopping</a>
-            </h1>
+            <Card style={{ padding: "20px", margin: "20px" }}>
+            <h3>
+              No Items in Cart.<a onClick={() => navigate("/")}>Go Shopping</a>
+            </h3>
+            </Card>
           ) : (
             <ListGroup>
               {cartItems.map((items) => (
-                <Card style={{ padding: "20px",margin:'20px' }}>
+                <Card style={{ padding: "20px", margin: "20px" }}>
                   <ListGroup.Item key={items._id}>
                     <Row className="align-items-center">
                       <Col md={2}>
@@ -50,8 +58,11 @@ function Bag() {
                         <span>Rs{items.price}</span>{" "}
                       </Col>
                       <Col md={2}>
-                        <Button variant="light">
-                            <DeleteIcon></DeleteIcon>
+                        <Button
+                          variant="light"
+                          onClick={() => removeItemHandler(items)}
+                        >
+                          <DeleteIcon></DeleteIcon>
                         </Button>
                       </Col>
                     </Row>
@@ -62,27 +73,36 @@ function Bag() {
           )}
         </Col>
         <Col md={4}>
-            <Card style={{ padding: "20px",margin:'20px' }}>
-                <ListGroup variant="flush">
-                    <ListGroup.Item >
-                        <h3>
-                            Subtotal ({cartItems.reduce((a,c)=> a + parseInt(c.input.quantity),0)}{' '}
-                            items) : Rs  
-                            {cartItems.reduce((a,c) => a + (parseInt(c.price) * parseInt(c.input.quantity)),0)}
-                        </h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item >
-                        <div className="d-grid">
-                            <Button
-                            type="button"
-                            disabled={cartItems.length === 0}
-                            style={{background:"rgba(232,28,101,0.83)"}}>
-                                Proceed To Checkout  
-                            </Button>
-                        </div>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Card>
+          <Card style={{ padding: "20px", margin: "20px" }}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>
+                  Subtotal (
+                  {cartItems.reduce(
+                    (a, c) => a + parseInt(c.input.quantity),
+                    0
+                  )}{" "}
+                  items) : Rs
+                  {cartItems.reduce(
+                    (a, c) =>
+                      a + parseInt(c.price) * parseInt(c.input.quantity),
+                    0
+                  )}
+                </h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div className="d-grid">
+                  <Button
+                    type="button"
+                    disabled={cartItems.length === 0}
+                    style={{ background: "rgba(232,28,101,0.83)" }}
+                  >
+                    Proceed To Checkout
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </div>
