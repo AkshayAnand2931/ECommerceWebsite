85e013d68ccccefb2f4a0ddbd46fb03bd322444f
chiman-09 <achintyakrishna2021@gmail.com>
2022-11-27 18:03:54 +0530

    profile

--- ecommerce/src/Components/Profile.js
+++ ecommerce/src/Components/Profile.js
@@ -1,41 +1,82 @@
-import React from 'react'
-import reducer from './reducer'
-import logger from 'use-reducer-logger'
-import axios from 'axios'
-import { useEffect,useReducer } from 'react'
+import React from "react";
+import reducer from "./reducer";
+import logger from "use-reducer-logger";
+import axios from "axios";
+import { useEffect, useReducer } from "react";
 
 const Profile = (props) => {
-  const [{loading,error,products},dispatch] = useReducer(logger(reducer),
-  {
-    products:[],
-    loading:true,
-    error:''
-  })
-  
-useEffect(()=>
-{
-  const fetchData = async () =>
-  {
-    dispatch({type:'FETCH_REQUEST'});
-    try
-    {
-      const result = await axios.get(`/api/users/${props.user}`);
-      dispatch({type:'FETCH_SUCCESS',payload: result.data})
-    }
-    catch(err)
-    {
-      dispatch({type:'FETCH_FAIL',payload: err.message})
-    }
-  }
-  fetchData();
-},[]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/users/${props.user}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <h2>{products.name}</h2>
+    <div className="page-content page-container" id="page-content">
+      <div className="padding">
+        <div className="row container d-flex justify-content-center">
+          <div className="col-xl-6 col-md-12">
+            <div className="card user-card-full">
+              <div className="row m-l-0 m-r-0">
+                <div className="col-sm-4 bg-c-lite-green user-profile">
+                  <div className="card-block text-center text-white">
+                    <div className="m-b-25">
+                      <img
+                        src="https://img.icons8.com/bubbles/100/000000/user.png"
+                        className="img-radius"
+                        alt="User-Profile-Image"
+                      />
+                    </div>
+                    <h6 className="f-w-600">{products.name}</h6>
+                    
+                    <i className=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                  </div>
+                </div>
+                <div className="col-sm-8">
+                  <div className="card-block">
+                    <h6 className="m-b-20 p-b-5 b-b-default f-w-600">
+                      Profile
+                    </h6>
+                    <div classNameName="row">
+                    <div className="col-sm-6">
+                        <p className="m-b-10 f-w-600">Name</p>
+                        <h6 className="text-muted f-w-400">{products.name}</h6>
+                      </div>
+                      <div className="col-sm-6">
+                        <p className="m-b-10 f-w-600">Email</p>
+                        <h6 className="text-muted f-w-400">{products.email}</h6>
+                      </div>
+                      <div className="col-sm-6">
+                        <p className="m-b-10 f-w-600">Phone</p>
+                        <h6 className="text-muted f-w-400">{products.phone}</h6>
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <p className="m-b-10 f-w-600">Address</p>
+                        <h6 className="text-muted f-w-400">{products.address}</h6>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
